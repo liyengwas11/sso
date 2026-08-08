add8a13356ccc56143e8226af7d2ea4d7f759e5d
liyengwas <solomonl@zazu.cloud>
2026-08-08 04:25:14 +0300

    fix images url

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,8 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+            // 'url' => env('APP_URL').'/storage/',
+            'url' => env('APP_URL') . '/storage/app/public',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
